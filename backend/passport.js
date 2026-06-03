1b78ae59c43e7c41bc8e39c1c952caf3a787d693
Ankita Yadav <ankitayadav476076@gmail.com>
2026-06-03 10:55:10 +0530

    changed

--- backend/passport.js
+++ backend/passport.js
@@ -5,11 +5,12 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.CLIENT_ID,
+      clientID: process.env.GOOGLE_CLIENT_ID,
 
-      clientSecret: process.env.CLIENT_SECRET,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 
-      callbackURL:"http://localhost:5000/auth/google/callback",
+      callbackURL:
+        "https://fashion-style-lue9.onrender.com/auth/google/callback",
     },
 
     async (accessToken, refreshToken, profile, done) => {
